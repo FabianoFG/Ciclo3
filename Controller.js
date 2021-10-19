1b000a96d836150b3f91d004c15c0f2cf952f0f4
FabianoFG <fabiano.f.gentil@gmail.com>
2021-10-18 21:10:49 -0300

    Segundo dia concluído

--- Controller.js
+++ Controller.js
@@ -1,12 +1,72 @@
 const express = require('express');
 const cors = require('cors');
 
+const models = require('./models');
+
 const app = express();
 app.use(cors());
+app.use(express.json());
+
+let cliente = models.Cliente;
+let itempedido = models.ItemPedido;
+let pedido = models.Pedido;
+let servico = models.Servico;
 
 app.get('/', function(req, res){
     res.send('Olá, mundo!');
 });
+
+app.post('/servicos', async(req, res) => {//este bloco cria novo serviço no model // async exige resposta
+    await servico.create(
+        req.body
+    ).then(function(){
+        return res.json({
+            error: false,
+            message: "Serviço criado com sucesso!"
+        })
+    }).catch(function(erro){
+        return res.status(400).json({
+            error: true,
+            message: "Não foi possível conectar!"
+        })
+    });
+});
+
+app.post('/clientes', async(req, res) => {
+        await cliente.create(
+            req.body
+    ).then(function(){
+        return res.json({
+            error: false,
+            message: "Cliente cadastrado com sucesso!"
+        })
+    }).catch(function(erro){
+        return res.status(400).jason({
+            error: true,
+            message: "Não foi possível conectar!"
+        })
+    });
+});
+
+// app.get('/pedidos', async(req, res) => {
+//         await pedido.create({
+//         data: new Date(),
+//         ClienteId: 11
+//     });
+//         await itempedido.create({
+//         PedidoId: 11,
+//         ServicoId: 10,
+//         quantidade: 1,
+//         valor: 0.00
+//     });
+//         await itempedido.create({
+//         PedidoId: 11,
+//         ServicoId: 12,
+//         quantidade: 1,
+//         valor: 0.00
+//     });
+//     res.send('Pedido realizado com sucesso!');
+// });
 
 app.get('/clientes', function(req, res){
     res.send('Seja bem-vindo(a) a ServicesTI');
--- models/itempedido.js
+++ models/itempedido.js
@@ -11,8 +11,8 @@ module.exports = (sequelize, DataTypes) => {
      */
     static associate(models) {
       // define association here
-      ItemPedido.belongsTo(model.Pedido);
-      ItemPedido.belongsTo(model.Servico);
+      ItemPedido.belongsTo(models.Pedido);
+      ItemPedido.belongsTo(models.Servico);
     }
   };
   ItemPedido.init({
